fed381223301ecd5d66367c18d3c47ffdba6125a
hamsteri <aloha3d@yandex.ru>
2023-02-25 01:27:36 +0300

    Added BottomNavigationView and Sign In Page

--- app/src/main/java/com/example/onlineshop/MainActivity.kt
+++ app/src/main/java/com/example/onlineshop/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.onlineshop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.onlineshop.feature.sign_in.SignInFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private val bottomBar: BottomNavigationView by lazy { findViewById(R.id.bnvBar) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomBar.itemIconTintList = null
+
+        supportFragmentManager.beginTransaction().replace(R.id.container, SignInFragment())
+            .commit()
     }
 }
---
+++ app/src/main/java/com/example/onlineshop/feature/sign_in/SignInFragment.kt
@@ -0,0 +1,14 @@
+package com.example.onlineshop.feature.sign_in
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.onlineshop.R
+
+class SignInFragment : Fragment(R.layout.fragment_sign_in) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
